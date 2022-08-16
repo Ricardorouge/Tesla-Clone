31c139720cf7e4dbd9c43ceeaf11f8913ac3651a
ricardo <ricky032929@gmail.com>
2022-08-16 13:51:56 -0400

    made it reusable

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ import CarItem from "./assets/components/CarItem";
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem/>
+      <CarItem
+        name={"Model X"}
+        tagline={"Order Online For"}
+        taglineCTA ={'Touchless Delivery'}
+        image={require("./assets/images/ModelX.jpeg")}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- assets/components/CarItem/index.js
+++ assets/components/CarItem/index.js
@@ -1,34 +1,43 @@
-import React from 'react'
-import {View, Text, ImageBackground} from 'react-native'
-import StyledButton from '../StyledButton'
-import styles from './styles'
+import React from "react";
+import { View, Text, ImageBackground } from "react-native";
+import StyledButton from "../StyledButton";
+import styles from "./styles";
 
+const CarItem = (props) => {
+    const { name, tagline, image, taglineCTA} = props
 
-const CarItem = (props) =>{
-    return (
-        <View style={styles.carContainer}>
-        <ImageBackground
-          source={require('../../images/ModelS.jpeg')}
-          style={styles.image}
-        />
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subTitle}>Starting at $69,420</Text>
-        </View>
-        <StyledButton
-         type='primary'
-         content = 'Custom Order'
-         onPress={()=>{
-            console.warn("Custom order was pressed")
-         }}/>
-         <StyledButton
-         type='secondary'
-         content = 'Existing Inventory'
-         onPress={()=>{
-            console.warn("Existing Inventory was pressed")
-         }}/>
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={image}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subTitle}>{tagline}{' '}
+        <Text style = {styles.subTitleCTA}>
+            {taglineCTA}
+        </Text>
+        </Text>
       </View>
-    )
-}
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content="Custom Order"
+          onPress={() => {
+            console.warn("Custom order was pressed");
+          }}
+        />
+        <StyledButton
+          type="secondary"
+          content="Existing Inventory"
+          onPress={() => {
+            console.warn("Existing Inventory was pressed");
+          }}
+        />
+      </View>
+    </View>
+  );
+};
 
-export default CarItem
+export default CarItem;
